4686db91ff07df541a38553e3fb82b9e5bfe6aee
Usama-Yasir <usamayasir32@gmail.com>
2022-04-24 14:39:14 +0500

    initial comit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import reactLogo from "../images/react-icon-small.png"
 export default function Navbar() {
     return (
         <nav> 
-            <img src= {reactLogo} alt="React Logo" className="nav--icon" />
+            <img src= {reactLogo} 
+            alt="React Logo" 
+            className="nav--icon" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
