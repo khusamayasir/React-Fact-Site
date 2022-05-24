1cfedd3b7a6f5d3de6d61312ae67edacbf402225
Khawaja Usama Yasir <66178232+khusamayasir@users.noreply.github.com>
2022-05-24 11:50:04 +0500

    updated Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,8 @@
 import React from "react"
 
-export default function Main() {
+export default function Main(props) {
     return (
-        <main>
+        <main classname={props.darkmode ? "dark" : ""}>
             <h1 className="main--title">Fun facts about React</h1>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
